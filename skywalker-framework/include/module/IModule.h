66a4f65589fbb788ad974ecd3c3994036b08a02d
shyfan <410713847@qq.com>
2023-07-28 10:24:07 +0800

    SkywalkerFramework 三大基础

---
+++ skywalker-framework/include/module/IModule.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+**文件: IModule.h
+**作者: shyfan
+**日期: 2023/07/28 10:04:52
+**功能: 模块接口
+*************************************************************************/
+
+#ifndef __SKYWALKER_FRAMEWORK_I_MODULE_H__
+#define __SKYWALKER_FRAMEWORK_I_MODULE_H__
+
+namespace Skywalker
+{
+    namespace Framework
+    {
+        struct IModule
+        {
+        };
+    }
+}
+
+#endif // __SKYWALKER_FRAMEWORK_I_MODULE_H__
---
+++ skywalker-framework/include/plugin/IPlugin.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+**文件: IPlugin.h
+**作者: shyfan
+**日期: 2023/07/28 10:01:51
+**功能: 插件接口
+*************************************************************************/
+
+#ifndef __SKYWALKER_FRAMEWORK_I_PLUGIN_H__
+#define __SKYWALKER_FRAMEWORK_I_PLUGIN_H__
+
+namespace Skywalker
+{
+    namespace Framework
+    {
+        struct IPlugin
+        {
+        };
+    }
+}
+
+#endif // __SKYWALKER_FRAMEWORK_I_PLUGIN_H__
