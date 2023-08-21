0496b94e1ec85002639b6e80068ae3896f1919e4
shyfan <410713847@qq.com>
2023-08-21 12:09:19 +0800

    增加 Loader

---
+++ SkywalkerServerFramework/Core/Loader/SSFLoader.h
@@ -0,0 +1,23 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Core\Loader\SSFLoader.h
+**作者: shyfan
+**日期: 2023/08/21 12:06:04
+**功能: 加载器
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_LOADER_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_LOADER_H__
+
+#include "Include/SSFCore.h"
+
+#include "Core/Object/SSFObject.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFOLoader : public SSFObject
+{
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif // __SKYWALKER_SERVER_FRAMEWORK_LOADER_H__
