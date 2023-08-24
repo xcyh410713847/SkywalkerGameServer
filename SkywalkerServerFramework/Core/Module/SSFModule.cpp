60a25575ff416b0ffb8710d3cfa2c616d30c38ad
shyfan <410713847@qq.com>
2023-08-24 16:50:07 +0800

    保存

--- SkywalkerServerFramework/Core/Module/SSFModule.cpp
+++ SkywalkerServerFramework/Core/Module/SSFModule.cpp
@@ -9,50 +9,6 @@
 
 SKYWALKER_SF_NAMESPACE_USE
 
-#pragma region Object
-
-void SSFOModule::Init(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Init
-}
-
-void SSFOModule::Awake(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Awake
-}
-
-void SSFOModule::Start(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Start
-}
-
-void SSFOModule::Tick(SSFObjectErrors &Errors, int DelayMS)
-{
-    // TODO Shyfan 插件管理器Tick
-}
-
-void SSFOModule::Stop(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Stop
-}
-
-void SSFOModule::Sleep(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Sleep
-}
-
-void SSFOModule::Destroy(SSFObjectErrors &Errors)
-{
-    // TODO Shyfan 插件管理器Destroy
-}
-
-void SSFOModule::Release(SSFObjectErrors &Errors)
-{
-    delete this;
-}
-
-#pragma endregion Object
-
 SSFOModule::SSFOModule(SKYWALKER_SF_PTR_PLUGIN_MANAGER InPluginManager)
     : PluginManager(InPluginManager)
 {
