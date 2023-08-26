623a7e3f4d618a2482d64083d6ff35414821e874
shyfan <410713847@qq.com>
2023-08-26 15:49:31 +0800

    增加网络套接字对象

---
+++ SkywalkerServerFramework/Plugin/Network/Object/SSFObject_NetworkSocket.cpp
@@ -0,0 +1,8 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Network\Object\SSFObject_NetworkSocket.cpp
+**作者: shyfan
+**日期: 2023/08/26 15:47:17
+**功能: 网络套接字对象
+*************************************************************************/
+
+#include "SSFObject_NetworkSocket.h"
---
+++ SkywalkerServerFramework/Plugin/Network/Object/SSFObject_NetworkSocket.h
@@ -0,0 +1,23 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Network\Object\SSFObject_NetworkSocket.h
+**作者: shyfan
+**日期: 2023/08/26 15:46:45
+**功能: 网络套接字对象
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_OBJECT_NETWORK_SOCKET_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_OBJECT_NETWORK_SOCKET_H__
+
+#include "Include/SSFCore.h"
+
+#include "Core/Object/SSFObject.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFOObject_NetworkSocket : public SSFObject
+{
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif // __SKYWALKER_SERVER_FRAMEWORK_OBJECT_NETWORK_SOCKET_H__
