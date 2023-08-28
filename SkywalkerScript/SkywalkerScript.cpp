44475ce286265551503ac0a88949ab83d06e383b
shyfan <410713847@qq.com>
2023-08-28 23:07:18 +0800

    增加Skywalker Script

---
+++ SkywalkerScript/SkywalkerScript.cpp
@@ -0,0 +1,15 @@
+﻿/*************************************************************************
+**文件: SkywalkerScript\SkywalkerScript.cpp
+**作者: shyfan
+**日期: 2023/08/28 23:01:35
+**功能: SkywalkerScript
+*************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello SkywalkerScript" << std::endl;
+
+    return 0;
+}
