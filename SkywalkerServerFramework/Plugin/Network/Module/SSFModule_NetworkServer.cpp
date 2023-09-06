ab87e8793c6961b0ef99d22fbca708494add9edb
shyfan <410713847@qq.com>
2023-09-06 20:34:53 +0800

    保存

--- SkywalkerServerFramework/Plugin/Network/Module/SSFModule_NetworkServer.cpp
+++ SkywalkerServerFramework/Plugin/Network/Module/SSFModule_NetworkServer.cpp
@@ -119,30 +119,6 @@ void SSFModule_NetworkServer::CreateNetworkServer(SSFNetworkErrors &Errors)
     }
 
     ServerNetworkSocket->Create(Errors, Context);
-
-    SOCKET ServerSocket = ServerNetworkSocket->GetSocket();
-
-    // 绑定IP地址和端口号
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(9527); // 设置端口号
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(ServerSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
-        SKYWALKER_SF_ERROR_DESC_TRACE(Errors, SkywalkerSFError_Network_Socket_BindFailed, "Failed to bind address to the socket")
-        closesocket(ServerSocket);
-        WSACleanup();
-        return;
-    }
-
-    // 监听和接受连接请求
-    if (listen(ServerSocket, SOMAXCONN) == SOCKET_ERROR)
-    {
-        SKYWALKER_SF_ERROR_DESC_TRACE(Errors, SkywalkerSFError_Network_Socket_ListenFailed, "Failed to listen")
-        closesocket(ServerSocket);
-        WSACleanup();
-        return;
-    }
 }
 
 void SSFModule_NetworkServer::CreateNetworkClient(SSFNetworkErrors &Errors)
--- SkywalkerServerFramework/Plugin/Network/Object/SSFObject_ServerSocket.cpp
+++ SkywalkerServerFramework/Plugin/Network/Object/SSFObject_ServerSocket.cpp
@@ -12,4 +12,28 @@ SKYWALKER_SF_NAMESPACE_USING
 void SSFObject_ServerSocket::Create(SSFObjectErrors &Errors, SSFNetworkSocketCreatorContext &Context)
 {
     SSFObject_NetworkSocket::Create(Errors, Context);
+
+    SOCKET ServerSocket = GetSocket();
+
+    // 绑定IP地址和端口号
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(9527); // 设置端口号
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(ServerSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+    {
+        SKYWALKER_SF_ERROR_DESC_TRACE(Errors, SkywalkerSFError_Network_Socket_BindFailed, "Failed to bind address to the socket")
+        closesocket(ServerSocket);
+        WSACleanup();
+        return;
+    }
+
+    // 监听和接受连接请求
+    if (listen(ServerSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        SKYWALKER_SF_ERROR_DESC_TRACE(Errors, SkywalkerSFError_Network_Socket_ListenFailed, "Failed to listen")
+        closesocket(ServerSocket);
+        WSACleanup();
+        return;
+    }
 }
