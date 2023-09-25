205b1bf4e37045fecff1623a0df65b7da0f241dd
Shyfan <410713847@qq.com>
2023-09-25 20:28:42 +0800

    新增插件 Profiler

---
+++ SkywalkerServerFramework/Plugin/Profiler/SSFPlugin_Profiler.cpp
@@ -0,0 +1,47 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Profiler\SSFPlugin_Profiler.cpp
+**作者: shyfan
+**日期: 2023/09/25 20:23:25
+**功能: 分析器插件
+*************************************************************************/
+
+#include "SSFPlugin_Profiler.h"
+
+#include "Include/SSFCore.h"
+#include "Include/SSFILog.h"
+
+SKYWALKER_SF_NAMESPACE_USING
+
+SKYWALKER_SF_LOG_DEFINE(SSFPlugin_Profiler, LogLevel_Debug);
+
+#ifdef SKYWALKER_SF_DYNAMIC_PLUGIN
+
+#define SKYWALKER_SF_PLUGIN_CLASS SSFPlugin_Profiler
+
+#pragma region 插件导出
+
+SKYWALKER_SF_DLL_EXPORT void DllStartPlugin(SKYWALKER_SF_PTR_PLUGIN_MANAGER PluginManager)
+{
+    SKYWALKER_SF_REGISTER_PLUGIN(PluginManager, SKYWALKER_SF_PLUGIN_CLASS);
+}
+
+SKYWALKER_SF_DLL_EXPORT void DllStopPlugin(SKYWALKER_SF_PTR_PLUGIN_MANAGER PluginManager)
+{
+    SKYWALKER_SF_UNREGISTER_PLUGIN(PluginManager, SKYWALKER_SF_PLUGIN_CLASS);
+}
+
+#pragma endregion 插件导出
+
+#endif // SKYWALKER_SF_DYNAMIC_PLUGIN
+
+#pragma region SSFOPlugin
+
+void SSFPlugin_Profiler::Install(SSFModuleErrors &Errors)
+{
+}
+
+void SSFPlugin_Profiler::Uninstall(SSFModuleErrors &Errors)
+{
+}
+
+#pragma endregion SSFOPlugin
---
+++ SkywalkerServerFramework/Plugin/Profiler/SSFPlugin_Profiler.h
@@ -0,0 +1,54 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Profiler\SSFPlugin_Profiler.h
+**作者: shyfan
+**日期: 2023/09/25 20:20:26
+**功能: 分析器插件
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_PLUGIN_PROFILER_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_PLUGIN_PROFILER_H__
+
+#include "Core/Plugin/SSFPluginManager.h"
+#include "Core/Plugin/SSFPlugin.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFPlugin_Profiler : public SSFOPlugin
+{
+    SSF_OBJECT_CLASS(SSFPlugin_Profiler)
+
+public:
+    SSFPlugin_Profiler(SKYWALKER_SF_PTR_PLUGIN_MANAGER InPluginManager)
+        : SSFOPlugin(InPluginManager)
+    {
+    }
+    virtual ~SSFPlugin_Profiler(){};
+
+    /**
+     * 获取插件名称
+     * @return 插件名称
+     */
+    virtual const std::string GetName() override
+    {
+        return SKYWALKER_SF_CLASS_NAME(SSFPlugin_Profiler);
+    };
+
+#pragma region SSFOPlugin
+
+private:
+    /**
+     * 安装
+     */
+    virtual void Install(SSFModuleErrors &Errors) override;
+
+    /**
+     * 卸载
+     */
+    virtual void Uninstall(SSFModuleErrors &Errors) override;
+
+#pragma endregion SSFOPlugin
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif // __SKYWALKER_SERVER_FRAMEWORK_PLUGIN_PROFILER_H__
