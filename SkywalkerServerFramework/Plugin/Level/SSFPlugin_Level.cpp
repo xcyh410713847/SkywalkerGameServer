56f7617bb427cd7d797022facb6c7d94c418c151
Shyfan <410713847@qq.com>
2023-09-26 20:53:50 +0800

    增加 Level 插件

---
+++ SkywalkerServerFramework/Plugin/Level/SSFPlugin_Level.cpp
@@ -0,0 +1,28 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Level\SSFPlugin_Level.cpp
+**作者: shyfan
+**日期: 2023/09/26 20:51:23
+**功能: 关卡插件
+*************************************************************************/
+
+#include "SSFPlugin_Level.h"
+
+#include "Include/SSFILog.h"
+
+SKYWALKER_SF_NAMESPACE_USING
+
+SKYWALKER_SF_LOG_DEFINE(SSFPlugin_Level, LogLevel_Debug);
+
+SKYWALKER_SF_PLUGIN_EXPORT(SSFPlugin_Level)
+
+#pragma region SSFOPlugin
+
+void SSFPlugin_Level::Install(SSFModuleErrors &Errors)
+{
+}
+
+void SSFPlugin_Level::Uninstall(SSFModuleErrors &Errors)
+{
+}
+
+#pragma endregion SSFOPlugin
---
+++ SkywalkerServerFramework/Plugin/Level/SSFPlugin_Level.h
@@ -0,0 +1,54 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Level\SSFPlugin_Level.h
+**作者: shyfan
+**日期: 2023/09/26 20:51:02
+**功能: 关卡插件
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Level_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Level_H__
+
+#include "Core/Plugin/SSFPluginManager.h"
+#include "Core/Plugin/SSFPlugin.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFPlugin_Level : public SSFOPlugin
+{
+    SSF_OBJECT_CLASS(SSFPlugin_Level)
+
+public:
+    SSFPlugin_Level(SKYWALKER_SF_PTR_PLUGIN_MANAGER InPluginManager)
+        : SSFOPlugin(InPluginManager)
+    {
+    }
+    virtual ~SSFPlugin_Level(){};
+
+    /**
+     * 获取插件名称
+     * @return 插件名称
+     */
+    virtual const std::string GetName() override
+    {
+        return SKYWALKER_SF_CLASS_NAME(SSFPlugin_Level);
+    }
+
+#pragma region SSFOPlugin
+
+private:
+    /**
+     * 安装
+     */
+    virtual void Install(SSFModuleErrors &Errors) override;
+
+    /**
+     * 卸载
+     */
+    virtual void Uninstall(SSFModuleErrors &Errors) override;
+
+#pragma endregion SSFOPlugin
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Level_H__
