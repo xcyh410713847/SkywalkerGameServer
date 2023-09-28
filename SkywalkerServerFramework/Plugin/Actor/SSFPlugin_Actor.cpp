b3f7dea21f957b74bf1c8ae35a4b2a4aed22a416
Shyfan <410713847@qq.com>
2023-09-28 10:21:36 +0800

    增加 Actor 插件

---
+++ SkywalkerServerFramework/Plugin/Actor/SSFPlugin_Actor.cpp
@@ -0,0 +1,28 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Actor\SSFPlugin_Actor.cpp
+**作者: shyfan
+**日期: 2023/09/28 10:15:05
+**功能: Actor插件
+*************************************************************************/
+
+#include "SSFPlugin_Actor.h"
+
+#include "Include/SSFILog.h"
+
+SKYWALKER_SF_NAMESPACE_USING
+
+SKYWALKER_SF_LOG_DEFINE(SSFPlugin_Actor, LogLevel_Debug);
+
+SKYWALKER_SF_PLUGIN_EXPORT(SSFPlugin_Actor)
+
+#pragma region SSFOPlugin
+
+void SSFPlugin_Actor::Install(SSFModuleErrors &Errors)
+{
+}
+
+void SSFPlugin_Actor::Uninstall(SSFModuleErrors &Errors)
+{
+}
+
+#pragma endregion SSFOPlugin
---
+++ SkywalkerServerFramework/Plugin/Actor/SSFPlugin_Actor.h
@@ -0,0 +1,54 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\Actor\SSFPlugin_Actor.h
+**作者: shyfan
+**日期: 2023/09/28 10:14:54
+**功能: Actor插件
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Actor_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Actor_H__
+
+#include "Core/Plugin/SSFPluginManager.h"
+#include "Core/Plugin/SSFPlugin.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFPlugin_Actor : public SSFOPlugin
+{
+    SSF_OBJECT_CLASS(SSFPlugin_Actor)
+
+public:
+    SSFPlugin_Actor(SKYWALKER_SF_PTR_PLUGIN_MANAGER InPluginManager)
+        : SSFOPlugin(InPluginManager)
+    {
+    }
+    virtual ~SSFPlugin_Actor(){};
+
+    /**
+     * 获取插件名称
+     * @return 插件名称
+     */
+    virtual const std::string GetName() override
+    {
+        return SKYWALKER_SF_CLASS_NAME(SSFPlugin_Actor);
+    }
+
+#pragma region SSFOPlugin
+
+private:
+    /**
+     * 安装
+     */
+    virtual void Install(SSFModuleErrors &Errors) override;
+
+    /**
+     * 卸载
+     */
+    virtual void Uninstall(SSFModuleErrors &Errors) override;
+
+#pragma endregion SSFOPlugin
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_Actor_H__
