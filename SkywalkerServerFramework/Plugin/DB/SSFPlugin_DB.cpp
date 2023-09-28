1d86029f1bf38ae3140f5ed102915544d8aa45e7
Shyfan <410713847@qq.com>
2023-09-28 10:49:14 +0800

    增加 DB 插件

---
+++ SkywalkerServerFramework/Plugin/DB/SSFPlugin_DB.cpp
@@ -0,0 +1,28 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\DB\SSFPlugin_DB.cpp
+**作者: shyfan
+**日期: 2023/09/28 10:47:44
+**功能: DB插件
+*************************************************************************/
+
+#include "SSFPlugin_DB.h"
+
+#include "Include/SSFILog.h"
+
+SKYWALKER_SF_NAMESPACE_USING
+
+SKYWALKER_SF_LOG_DEFINE(SSFPlugin_DB, LogLevel_Debug);
+
+SKYWALKER_SF_PLUGIN_EXPORT(SSFPlugin_DB)
+
+#pragma region SSFOPlugin
+
+void SSFPlugin_DB::Install(SSFModuleErrors &Errors)
+{
+}
+
+void SSFPlugin_DB::Uninstall(SSFModuleErrors &Errors)
+{
+}
+
+#pragma endregion SSFOPlugin
---
+++ SkywalkerServerFramework/Plugin/DB/SSFPlugin_DB.h
@@ -0,0 +1,54 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\DB\SSFPlugin_DB.h
+**作者: shyfan
+**日期: 2023/09/28 10:47:30
+**功能: DB插件
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_DB_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_DB_H__
+
+#include "Core/Plugin/SSFPluginManager.h"
+#include "Core/Plugin/SSFPlugin.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFPlugin_DB : public SSFOPlugin
+{
+    SSF_OBJECT_CLASS(SSFPlugin_DB)
+
+public:
+    SSFPlugin_DB(SKYWALKER_SF_PTR_PLUGIN_MANAGER InPluginManager)
+        : SSFOPlugin(InPluginManager)
+    {
+    }
+    virtual ~SSFPlugin_DB(){};
+
+    /**
+     * 获取插件名称
+     * @return 插件名称
+     */
+    virtual const std::string GetName() override
+    {
+        return SKYWALKER_SF_CLASS_NAME(SSFPlugin_DB);
+    }
+
+#pragma region SSFOPlugin
+
+private:
+    /**
+     * 安装
+     */
+    virtual void Install(SSFModuleErrors &Errors) override;
+
+    /**
+     * 卸载
+     */
+    virtual void Uninstall(SSFModuleErrors &Errors) override;
+
+#pragma endregion SSFOPlugin
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_DB_H__
