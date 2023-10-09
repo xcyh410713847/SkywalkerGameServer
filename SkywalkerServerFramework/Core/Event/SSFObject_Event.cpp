bd0f5227fb9952a3dd3aaa8eab86f78364c64cef
Shyfan <410713847@qq.com>
2023-10-09 20:37:23 +0800

    保存

---
+++ SkywalkerServerFramework/Core/Event/SSFObject_Event.cpp
@@ -0,0 +1,25 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Core\Event\SSFObject_Event.cpp
+**作者: shyfan
+**日期: 2023/10/09 20:33:32
+**功能: 事件对象
+*************************************************************************/
+
+#include "SSFObject_Event.h"
+
+#include "Include/SSFCore.h"
+#include "Include/SSFILog.h"
+
+SKYWALKER_SF_NAMESPACE_USING
+
+SKYWALKER_SF_LOG_DEFINE(SSFObject_Event, LogLevel_Debug);
+
+SSFObject_Event::SSFObject_Event()
+    : SSFObjectSimple()
+{
+    EnablePool = true;
+}
+
+SSFObject_Event::~SSFObject_Event()
+{
+}
---
+++ SkywalkerServerFramework/Core/Event/SSFObject_Event.h
@@ -0,0 +1,28 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Core\Event\SSFObject_Event.h
+**作者: shyfan
+**日期: 2023/10/09 20:33:06
+**功能: 事件对象
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFObject_Event_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFObject_Event_H__
+
+#include "Include/SSFCore.h"
+
+#include "Core/Object/SSFObjectSimple.h"
+
+SKYWALKER_SF_NAMESPACE_BEGIN
+
+class SSFObject_Event : public SSFObjectSimple
+{
+    SSF_OBJECT_CLASS(SSFObject_Event);
+
+public:
+    SSFObject_Event();
+    virtual ~SSFObject_Event();
+};
+
+SKYWALKER_SF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFObject_Event_H__
