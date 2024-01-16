6b4861035b9a047fd64aed0dda38176f0c5193e7
Shyfan <410713847@qq.com>
2024-01-16 12:07:53 +0800

    增加新的插件

---
+++ SkywalkerServerFramework/Plugin/CommandLine/SSFPlugin_CommandLine.cpp
@@ -0,0 +1,30 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\CommandLine\SSFPlugin_CommandLine.cpp
+**作者: shyfan
+**日期: 2024/01/16 11:47:43
+**功能: 命令行插件
+*************************************************************************/
+
+#include "SSFPlugin_CommandLine.h"
+
+#include "Include/SSFILog.h"
+
+#include "Core/Plugin/SSFPluginManager.h"
+
+SSF_NAMESPACE_USING
+
+SSF_LOG_DEFINE(SSFPlugin_CommandLine, LogLevel_Debug);
+
+SSF_PLUGIN_EXPORT(SSFPlugin_CommandLine)
+
+#pragma region SSFOPlugin
+
+void SSFPlugin_CommandLine::Install(SSFModuleErrors &Errors)
+{
+}
+
+void SSFPlugin_CommandLine::Uninstall(SSFModuleErrors &Errors)
+{
+}
+
+#pragma endregion SSFOPlugin
---
+++ SkywalkerServerFramework/Plugin/CommandLine/SSFPlugin_CommandLine.h
@@ -0,0 +1,44 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Plugin\CommandLine\SSFPlugin_CommandLine.h
+**作者: shyfan
+**日期: 2024/01/16 11:45:30
+**功能: 命令行插件
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_CommandLine_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_CommandLine_H__
+
+#include "Core/Plugin/SSFPlugin.h"
+
+SSF_NAMESPACE_BEGIN
+
+class SSFPlugin_CommandLine : public SSFOPlugin
+{
+    SSF_OBJECT_CLASS(SSFPlugin_CommandLine)
+
+public:
+    SSFPlugin_CommandLine(SSF_PTR_PLUGIN_MANAGER InPluginManager)
+        : SSFOPlugin(InPluginManager)
+    {
+    }
+    virtual ~SSFPlugin_CommandLine(){};
+
+#pragma region SSFOPlugin
+
+private:
+    /**
+     * 安装
+     */
+    virtual void Install(SSFModuleErrors &Errors) override;
+
+    /**
+     * 卸载
+     */
+    virtual void Uninstall(SSFModuleErrors &Errors) override;
+
+#pragma endregion SSFOPlugin
+};
+
+SSF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFPlugin_CommandLine_H__
