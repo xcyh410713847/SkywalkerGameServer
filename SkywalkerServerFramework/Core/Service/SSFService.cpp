7f9137c193b4ad598189bc01d886c7042ba5e8a3
Shyfan <410713847@qq.com>
2024-01-16 21:12:12 +0800

    保存

---
+++ SkywalkerServerFramework/Core/Service/SSFService.cpp
@@ -0,0 +1,25 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Core\Service\SSFService.cpp
+**作者: shyfan
+**日期: 2024/01/16 21:10:33
+**功能: SSFService
+*************************************************************************/
+
+#include "SSFService.h"
+
+#include "Include/SSFILog.h"
+
+SSF_NAMESPACE_USING
+
+SSF_LOG_DEFINE(SSFService, LogLevel_Debug);
+
+#define SSF_LOG_DEBUG_MODULE(content) SSF_LOG_DEBUG(GetObjectClassName() << " " << content)
+
+SSFService::SSFService(SSFObjectContext &InContext, SSFObjectErrors &InErrors)
+    : SSFObject(InContext, InErrors)
+{
+}
+
+SSFService::~SSFService()
+{
+}
---
+++ SkywalkerServerFramework/Core/Service/SSFService.h
@@ -0,0 +1,27 @@
+﻿/*************************************************************************
+**文件: SkywalkerServerFramework\Core\Service\SSFService.h
+**作者: shyfan
+**日期: 2024/01/16 21:10:23
+**功能: SSFService
+*************************************************************************/
+
+#ifndef __SKYWALKER_SERVER_FRAMEWORK_SSFService_H__
+#define __SKYWALKER_SERVER_FRAMEWORK_SSFService_H__
+
+#include "Include/SSFCore.h"
+
+#include "Core/Object/SSFObject.h"
+
+SSF_NAMESPACE_BEGIN
+
+class SSFService : public SSFObject
+{
+    SSF_OBJECT_CLASS(SSFService)
+
+    SSFService(SSFObjectContext &InContext, SSFObjectErrors &InErrors);
+    virtual ~SSFService();
+};
+
+SSF_NAMESPACE_END
+
+#endif //__SKYWALKER_SERVER_FRAMEWORK_SSFService_H__
