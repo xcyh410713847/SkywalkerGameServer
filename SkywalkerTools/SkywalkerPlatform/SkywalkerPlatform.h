6a27049c4cb72b8ebdfe98929b4a62a831389c94
shyfan <shyfan@skywalker.com>
2026-02-26 20:27:12 +0800

    refactor: 增强跨平台支持
    - 添加平台名称宏 SKYWALKER_PLATFORM_NAME
    - 添加线程本地存储宏 SKYWALKER_THREAD_LOCAL
    - 添加路径分隔符宏 SKYWALKER_PATH_SEPARATOR
    - 添加禁用拷贝宏 SKYWALKER_DISALLOW_COPY
    - 支持 Windows/iOS/Mac/Linux 平台检测

--- SkywalkerTools/SkywalkerPlatform/SkywalkerPlatform.h
+++ SkywalkerTools/SkywalkerPlatform/SkywalkerPlatform.h
@@ -1,4 +1,4 @@
-﻿/*************************************************************************
+/*************************************************************************
 **文件: SkywalkerTools\SkywalkerPlatform\SkywalkerPlatform.h
 **作者: shyfan
 **日期: 2023/08/04 16:57:26
@@ -26,4 +26,35 @@
 #define SKYWALKER_DYNAMIC_LIB_EXT ".so"
 #endif
 
-#endif // __SKYWALKER_PLATFORM_H__
+#if defined(_WIN32) || defined(_WIN64)
+    #define SKYWALKER_PLATFORM_NAME "Windows"
+#elif defined(__APPLE__)
+    #include <TargetConditionals.h>
+    #if defined(TARGET_OS_IPHONE)
+        #define SKYWALKER_PLATFORM_NAME "iOS"
+    #else
+        #define SKYWALKER_PLATFORM_NAME "Mac"
+    #endif
+#elif defined(__linux__)
+    #define SKYWALKER_PLATFORM_NAME "Linux"
+#else
+    #define SKYWALKER_PLATFORM_NAME "Unknown"
+#endif
+
+#if defined(SKYWALKER_PLATFORM_WINDOWS)
+    #define SKYWALKER_THREAD_LOCAL __declspec(thread)
+#else
+    #define SKYWALKER_THREAD_LOCAL __thread
+#endif
+
+#if defined(SKYWALKER_PLATFORM_WINDOWS)
+    #define SKYWALKER_PATH_SEPARATOR '\\'
+#else
+    #define SKYWALKER_PATH_SEPARATOR '/'
+#endif
+
+#define SKYWALKER_DISALLOW_COPY(ClassName) \
+    ClassName(const ClassName&) = delete; \
+    ClassName& operator=(const ClassName&) = delete;
+
+#endif
